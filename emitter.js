7a180d027249688f1c59057d717712157a9f7cac
Aleksey <AllEXEcuted>
2018-12-11 18:25:54 +0500

    test

--- emitter.js
+++ emitter.js
@@ -4,40 +4,74 @@
  * Сделано задание на звездочку
  * Реализованы методы several и through
  */
-const isStar = true;
+const isStar = false;
 
 /**
  * Возвращает новый emitter
  * @returns {Object}
  */
+
 function getEmitter() {
+    const allEvents = new Map();
+
     return {
 
         /**
          * Подписаться на событие
-         * @param {String} event
+         * @param {String} newEvent
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object}
          */
-        on: function (event, context, handler) {
-            console.info(event, context, handler);
+        on: function (newEvent, context, handler) {
+            if (!allEvents.get(newEvent)) {
+                allEvents.set(newEvent, new Map());
+            }
+
+            if (!allEvents.get(newEvent).get(context)) {
+                allEvents.get(newEvent).set(context, []);
+            }
+
+            allEvents.get(newEvent).get(context)
+                .push(handler);
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object}
          */
         off: function (event, context) {
-            console.info(event, context);
+            Array.from(allEvents.entries())
+                .filter((value)=>value[0] === event || value[0].startsWith(`${event}.`))
+                .map((value)=>allEvents.get(value[0]))
+                .forEach((item)=>item.delete(context));
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object}
          */
         emit: function (event) {
-            console.info(event);
+            event.split('.').map((item, i, arr)=>arr.slice(0, i + 1).join('.'))
+                .reverse()
+                .forEach(newEvent => {
+                    if (allEvents.get(newEvent)) {
+                        allEvents.get(newEvent).forEach((handlers, context)=> {
+                            handlers.forEach(heandler=> {
+                                heandler.call(context);
+                            });
+                        });
+                    }
+                });
+
+            return this;
         },
 
         /**
